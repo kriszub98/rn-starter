f723390f85db11d339c07d012aa8b40f4180d768
kzubrzycki <kriszub98@gmail.com>
2021-05-05 23:11:42 +0200

    First task!

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-	const greeting = <Text>Hi there!</Text>;
-
+	const yourname = 'Christopher';
 	return (
 		<View>
-			<Text style={styles.textStyle}>This is the components screen</Text>
-			{greeting}
+			<Text style={styles.textStyle}>Getting started with React Native!</Text>
+			<Text style={styles.smallTextStyle}>My name is {yourname}</Text>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	textStyle: {
-		fontSize: 30
+		fontSize: 45
+	},
+	smallTextStyle: {
+		fontSize: 20
 	}
 });
 
