53d226ed81c5a4517c5e5b3c5bb5f8054449fcdd
kzubrzycki <kriszub98@gmail.com>
2021-05-08 01:53:23 +0200

    Button primitive usage

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,24 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, Button, View } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>It just works!</Text>;
+	return (
+		<View>
+			<Text style={styles.text}>It just works!</Text>
+			<Button
+				title="Go to Components Demo"
+				onPress={() => {
+					console.log('Button pressed');
+				}}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+	text: {
+		fontSize: 30
+	}
 });
 
 export default HomeScreen;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,15 +3,12 @@ import { FlatList, StyleSheet, View, Text } from 'react-native';
 
 const ListScreen = () => {
 	const friends = [
-		{ name: 'Friend #1' },
-		{ name: 'Friend #2' },
-		{ name: 'Friend #3' },
-		{ name: 'Friend #4' },
-		{ name: 'Friend #5' },
-		{ name: 'Friend #6' },
-		{ name: 'Friend #7' },
-		{ name: 'Friend #8' },
-		{ name: 'Friend #9' }
+		{ name: 'Friend #1', age: 20 },
+		{ name: 'Friend #2', age: 45 },
+		{ name: 'Friend #3', age: 32 },
+		{ name: 'Friend #4', age: 27 },
+		{ name: 'Friend #5', age: 53 },
+		{ name: 'Friend #6', age: 30 }
 	];
 
 	return (
@@ -22,14 +19,18 @@ const ListScreen = () => {
 			data={friends}
 			renderItem={({ item }) => {
 				//item === {name: 'Friend #1'}
-				return <Text style={styles.textStyle}>{item.name}</Text>;
+				return (
+					<Text style={styles.textStyle}>
+						{item.name} - Age {item.age}
+					</Text>
+				);
 			}}
 		/>
 	);
 };
 
 const styles = StyleSheet.create({
-	textStyle: { fontSize: 35 }
+	textStyle: { marginVertical: 50 }
 });
 
 export default ListScreen;
