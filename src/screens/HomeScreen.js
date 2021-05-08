952dbd4b43ae5a341efaeea3583e1b54448ef848
kzubrzycki <kriszub98@gmail.com>
2021-05-08 02:15:22 +0200

    Basic navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet, Button, View } from 'react-native';
+import { Text, StyleSheet, Button, View, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+	console.log(props.navigation);
 	return (
 		<View>
 			<Text style={styles.text}>It just works!</Text>
 			<Button
 				title="Go to Components Demo"
 				onPress={() => {
-					console.log('Button pressed');
+					props.navigation.navigate('Components');
 				}}
 			/>
+			<TouchableOpacity
+				onPress={() => {
+					props.navigation.navigate('List');
+				}}
+			>
+				<Text style={styles.text}>Go to List Demo</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
