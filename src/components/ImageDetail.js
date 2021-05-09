ce9c5b3f0a71ede50c373c10670738f4788b1166
kzubrzycki <kriszub98@gmail.com>
2021-05-09 02:05:10 +0200

    Passing props and showing images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = () => {
-	return <Text>ImageDetail</Text>;
+const ImageDetail = (props) => {
+	return (
+		<View>
+			<Image source={require('../../assets/beach.jpg')} />
+			<Text>{props.title}</Text>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({});
