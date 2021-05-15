cf91551e903e24d9c8ca584875dcd4fba0c1724f
kzubrzycki <kriszub98@gmail.com>
2021-05-15 09:45:08 +0200

    First State managed component

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+
+const CounterScreen = () => {
+	const [ counter, setCounter ] = useState(0);
+	return (
+		<View>
+			<Button
+				title="Increase"
+				onPress={() => {
+					// Don't modify the state variable directly
+					setCounter(counter + 1);
+				}}
+			/>
+			<Button
+				title="Decrease"
+				onPress={() => {
+					setCounter(counter - 1);
+				}}
+			/>
+			<Text>Current Count: </Text>
+			<Text>{counter}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
