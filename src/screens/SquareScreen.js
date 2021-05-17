3236b6d00e0de4ed05587a3fe1cf154cd55ed9ee
kzubrzycki <kriszub98@gmail.com>
2021-05-17 10:14:20 +0200

    Refactoring squareScreen, add validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,12 +9,32 @@ const SquareScreen = () => {
 	const [ green, setGreen ] = useState(0);
 	const [ blue, setBlue ] = useState(0);
 
+	const setColor = (color, change) => {
+		// color === 'red', 'green', 'blue'
+		// change === +COLOR_INCREMENT, -COLOR_INCREMENT
+
+		switch (color) {
+			case 'red':
+				if (red + change > 255 || red - change < 0) return;
+				else setRed(red + change);
+				return;
+			case 'green':
+				if (green + change > 255 || green - change < 0) return;
+				else setRed(green + change);
+				return;
+			case 'blue':
+				if (blue + change > 255 || blue - change < 0) return;
+				else setRed(blue + change);
+				return;
+		}
+	};
+
 	return (
 		<View>
 			<ColorCounter
 				color="Red"
-				onIncrease={() => setRed(red + COLOR_INCREMENT)}
-				onDecrease={() => setRed(red - COLOR_INCREMENT)}
+				onIncrease={() => setColor('red', COLOR_INCREMENT)}
+				onDecrease={() => setColor('red', -COLOR_INCREMENT)}
 			/>
 			<ColorCounter
 				color="Green"
