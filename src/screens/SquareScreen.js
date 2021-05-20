e3fff64515a9c2f33e7d538549869750a2cceb3c
kzubrzycki <kriszub98@gmail.com>
2021-05-20 20:45:29 +0200

    readded validation

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -10,10 +10,19 @@ const reducer = (state, action) => {
 	switch (action.colorToChange) {
 		case 'red':
 			//Copy all properties and overwrite red color
+			if (state.red + action.amount > 255 || state.red + action.amount < 0) {
+				return state; // We always must return value from reducers!!! if we dont its undefined after rerender
+			}
 			return { ...state, red: state.red + action.amount };
 		case 'green':
+			if (state.green + action.amount > 255 || state.green + action.amount < 0) {
+				return state; // We always must return value from reducers!!! if we dont its undefined after rerender
+			}
 			return { ...state, green: state.green + action.amount };
 		case 'blue':
+			if (state.blue + action.amount > 255 || state.blue + action.amount < 0) {
+				return state; // We always must return value from reducers!!! if we dont its undefined after rerender
+			}
 			return { ...state, blue: state.blue + action.amount };
 		default:
 			return state;
