fc156a28bc793c0b29a1ef87fc6b221dd46d6209
kzubrzycki <kriszub98@gmail.com>
2021-05-22 21:11:50 +0200

    Using state inside TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TextComponent } from 'react-native';
 
 const TextScreen = () => {
+	const [ name, setName ] = useState('');
+	// Inside value of textInput we are going to tell what its initial value should be!
+	// We do that to have access to its value inside parent component, normal value does not change state inside parent
+	// and we do not want parent to access its child property!
 	return (
 		<View>
 			<Text>Input your name:</Text>
-			<TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
+			<TextInput
+				style={styles.input}
+				autoCapitalize="none"
+				autoCorrect={false}
+				value={name}
+				onChangeText={(newValue) => setName(newValue)}
+			/>
+			<Text>{name}</Text>
 		</View>
 	);
 };
